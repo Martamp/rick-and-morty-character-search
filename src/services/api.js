675f1f31741589683637037c7a03788ca8247e7a
Martamp <motymont@gmail.com>
2020-04-22 21:37:33 +0200

    Get fetch data

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,23 @@
 const getApiData = () => {
   return fetch('https://rickandmortyapi.com/api/character')
-    .then(function (response) {
-      return response.json();
-    })
-    .catch(function (error) {
-      console.log(error);
+    .then((response) => response.json())
+    .then((data) => {
+      return data.results.map((character) => {
+        console.log(character);
+        return {
+          id: character.id,
+          name: character.name,
+          img: character.image,
+          species: character.species,
+          status: character.status,
+          origin: character.origin.name,
+          episodes: character.episode.length,
+        };
+      });
     });
 };
 
 export default getApiData;
+//  .catch(function (error) {
+//       console.log(error);
+//     });
