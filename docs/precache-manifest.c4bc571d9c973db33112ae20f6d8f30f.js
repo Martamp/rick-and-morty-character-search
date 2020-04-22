730d352c49e92747b60db785ab4fbc98615d0125
Martamp <motymont@gmail.com>
2020-04-22 17:58:03 +0200

    Create Docs folder for Github Pages

---
+++ docs/precache-manifest.c4bc571d9c973db33112ae20f6d8f30f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc54f85b2996bc9fc509930c2e6afaf7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dad998e2299e30a5ec2a",
+    "url": "./static/css/main.af6af2b6.chunk.css"
+  },
+  {
+    "revision": "65494b9e0bb6c27260e5",
+    "url": "./static/js/2.1dfebd2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1dfebd2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad998e2299e30a5ec2a",
+    "url": "./static/js/main.10764a8d.chunk.js"
+  },
+  {
+    "revision": "6a77c3a64167c31507ce",
+    "url": "./static/js/runtime-main.9885c3ac.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo-rick-and-morty.75509f2e.png"
+  },
+  {
+    "revision": "af2972d97e771d868e2bc6b0f966b918",
+    "url": "./static/media/prueba.af2972d9.png"
+  }
+]);
